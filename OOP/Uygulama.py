dae9e6ded4eaf909a38e446990bf514921f9154b
İbrahim Ediz <Kurs>
2020-02-09 15:19:54 +0200

    09022020_2

---
+++ OOP/Uygulama.py
@@ -0,0 +1,19 @@
+class MarverHero:
+    tip = "Marvel"
+    def __init__(self,guc,saglik,isim):
+        self.guc = guc
+        self.saglik = saglik
+        self.isim = isim
+    
+    def vur(self):
+        return self.guc
+
+    def darbe(self,guc):
+        self.saglik -= guc
+        return self.saglik
+    
+P1 = MarverHero(100,500,"DeadPool")
+P2 = MarverHero(50,450,"IronMan")
+print(P1.darbe(P2.vur()))
+print(f"{P2.isim} ==> {P1.isim} vurdu :\
+    {P1.isim} Saglık:{P1.saglik} {P2.isim} Saglık:{P2.saglik}")
